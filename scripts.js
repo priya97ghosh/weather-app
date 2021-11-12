79d440f151e0977b98369ebbccb0bea75809e38a
Priya Ghosh <41378468+priya97ghosh@users.noreply.github.com>
2021-11-12 17:47:47 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,40 +1,120 @@
-// Declaring the variables
-let lon;
-let lat;
-let temperature = document.querySelector(".temp");
-let summary = document.querySelector(".summary");
-let loc = document.querySelector(".location");
-let icon = document.querySelector(".icon");
-const kelvin = 273;
+const mylocation = document.querySelector(".mylocation");
+const temp = document.querySelector(".temprature");
+const celcius = document.querySelector(".celcius");
+const climate = document.querySelector(".climate");
+const tempIcon = document.querySelector(".tempIcon");
 
+const waitingArea = document.querySelector(".waitingArea");
+const printArea = document.querySelector(".printArea");
+const apiKey = "632c8f68bce80284a9943e8a28aa9cc8";
 window.addEventListener("load", () => {
+  const searchInput = document.querySelector(".searchInput");
+  printArea.style.display = "none";
+  waitingArea.style.display = "block";
+  celcius.style.display = "none";
+  searchInput.value = "";
+  let long;
+  let lat;
+  let id;
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log(position);
-      lon = position.coords.longitude;
+      long = position.coords.longitude;
       lat = position.coords.latitude;
+      // console.log("long", long, "lat", lat);
 
-      // API ID
-      const api = "6d055e39ee237af35ca066f35474e9df";
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}`;
 
-      // API URL
-      const base =
-        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
-        `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
-
-      // Calling the API
-      fetch(base)
+      fetch(api)
         .then((response) => {
           return response.json();
         })
         .then((data) => {
-          console.log(data);
-          temperature.textContent = Math.floor(data.main.temp - kelvin) + "°C";
-          summary.textContent = data.weather[0].description;
-          loc.textContent = data.name + "," + data.sys.country;
-          let icon1 = data.weather[0].icon;
-          icon.innerHTML = `<img src="https://nomadicways.travel/show/paarl-paarl/blog-image/Cloudy/${icon1}.png" style= 'height:10rem'/>`;
+          printArea.style.display = "block";
+          waitingArea.style.display = "none";
+          mylocation.textContent = data.name;
+          temp.textContent = Math.round(data.main.feels_like - 273);
+          celcius.style.display = "block";
+          climate.textContent = data.weather[0].main;
+          id = data.weather[0].id;
+
+          if (id < 250) {
+            tempIcon.src = "./icons/storm.svg";
+          }
+          if (id < 350) {
+            tempIcon.src = "./icons/drizzle.svg";
+          }
+          if (id < 650) {
+            tempIcon.src = "./icons/snow.svg";
+          }
+          if (id < 550) {
+            tempIcon.src = "./icons/rain.svg";
+          }
+          if (id < 800) {
+            tempIcon.src = "./icons/atmosphere.svg";
+          }
+          if (id === 800) {
+            tempIcon.src = "./icons/sun.svg";
+          }
+          if (id > 800) {
+            tempIcon.src = "./icons/clouds.svg";
+          }
+
+          // console.log(data);
+        })
+        .catch(() => {
+          alert("Please search for a valid city 😩");
         });
     });
   }
+});
+
+// By Search
+const searchBtn = document.querySelector(".searchBtn");
+searchBtn.addEventListener("click", () => {
+  const searchValue = document.querySelector(".searchInput").value;
+  printArea.style.display = "none";
+  waitingArea.style.display = "block";
+  // console.log(searchValue);
+
+  const searchApi = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=${apiKey}`;
+  fetch(searchApi)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      printArea.style.display = "block";
+      waitingArea.style.display = "none";
+      mylocation.textContent = data.name;
+      temp.textContent = Math.round(data.main.feels_like - 273);
+      climate.textContent = data.weather[0].main;
+      id = data.weather[0].id;
+
+      if (id < 250) {
+        tempIcon.src = "./icons/storm.svg";
+      }
+      if (id < 350) {
+        tempIcon.src = "./icons/drizzle.svg";
+      }
+      if (id < 650) {
+        tempIcon.src = "./icons/snow.svg";
+      }
+      if (id < 550) {
+        tempIcon.src = "./icons/rain.svg";
+      }
+      if (id < 800) {
+        tempIcon.src = "./icons/atmosphere.svg";
+      }
+      if (id === 800) {
+        tempIcon.src = "./icons/sun.svg";
+      }
+      if (id > 800) {
+        tempIcon.src = "./icons/clouds.svg";
+      }
+
+      // console.log(data);
+    })
+    .catch(() => {
+      alert("Please search for a valid city 😩");
+    });
 });
